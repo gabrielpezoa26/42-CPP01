bcf4601b8047a2784d6a96272954ebd803e6b2b4
gcesar-n <gcesar-n@student.42.ft>
2025-10-08 14:53:03 -0300

    added Makefile, Classes, headers and .cpp files

---
+++ ex00/Zombie.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gcesar-n <gcesar-n@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/08 14:52:12 by gcesar-n          #+#    #+#             */
+/*   Updated: 2025/10/08 14:52:12 by gcesar-n         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+#include <iostream>
+#include <new>
+
+class Zombie
+{
+	private:
+	std::string name;
+
+
+	public:
+	Zombie(std::string name);
+	~Zombie();
+	void announce(void);
+};
+
+Zombie *newZombie(std::string name);
+void randomChump(std::string name);
+
+#endif
