6775652abff329c64fa9e5fac9e7f2296712a956
Gabriel Pezoa <gabrielpezoa26@gmail.com>
2025-10-09 17:59:57 -0300

    leak fixed, ex00 almost finished

--- ex00/Zombie.cpp
+++ ex00/Zombie.cpp
@@ -6,13 +6,18 @@
 /*   By: gcesar-n <gcesar-n@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/08 14:52:03 by gcesar-n          #+#    #+#             */
-/*   Updated: 2025/10/09 15:44:12 by gcesar-n         ###   ########.fr       */
+/*   Updated: 2025/10/09 17:53:59 by gcesar-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
+Zombie::Zombie(std::string name)
+{
+	std::string mango = name;
+}
+
 Zombie::~Zombie()
 {
-	std::cout << "DEBUG: Destructor called" << name << std::endl;
+	std::cout << name << "deallocated" << std::endl;
 }
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,16 +6,11 @@
 /*   By: gcesar-n <gcesar-n@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/08 14:52:00 by gcesar-n          #+#    #+#             */
-/*   Updated: 2025/10/09 16:19:13 by gcesar-n         ###   ########.fr       */
+/*   Updated: 2025/10/09 17:59:07 by gcesar-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
-
-Zombie::Zombie(std::string name)
-{
-	std::string mango = name;
-}
 
 static void printMessage(std::string to_print, int flag)
 {
@@ -25,27 +20,41 @@ static void printMessage(std::string to_print, int flag)
 		std::cout << to_print;
 }
 
-static void debugPrint(std::string identifier, std::string to_debug)
+// static void debugPrint(std::string identifier, std::string to_debug)
+// {
+// 	std::cout << "DEBUG: "<< identifier << to_debug << std::endl;
+// }
+
+static void printDelay()
 {
-	std::cout << "DEBUG: "<< identifier << to_debug << std::endl;
+	printMessage("manufactoring zombies...", 0);
+	// sleep(2);
+	// printMessage("...", 0);
+	// sleep(3);
+	// printMessage("...", 0);
+	// sleep(3);
+	printMessage("zombies are ready!", 0);
+	sleep(2);
 }
 
 int main()
 {
 	std::string heapZombie;
 	std::string stackZombie;
+	Zombie *zombie_Pointer;
 
 	printMessage("How do you want to name your *Heap*-living zombie? ", 1);
 	std::getline(std::cin, heapZombie);
 
 	printMessage("How do you want to name your *Stack*-living zombie? ", 1);
 	std::getline(std::cin, stackZombie);
-	
-	debugPrint( "heap zombie: ", heapZombie);
-	debugPrint( "stack zombie: ", stackZombie);
-	
-	newZombie(heapZombie);
+
+	printDelay();
+
+	zombie_Pointer = newZombie(heapZombie);
+	std::cout << "zombie " << heapZombie << " is allocated" << std::endl;
 	randomChump(stackZombie);
-	// delete heapZombie;
+	delete zombie_Pointer;
+
 	return 0;
 }
