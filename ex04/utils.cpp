c3abfe6671a47075c791c0b8e47b5716ca626227
Gabriel Pezoa <gabrielpezoa26@gmail.com>
2025-10-20 17:14:15 -0300

    finished ex04

--- ex04/utils.cpp
+++ ex04/utils.cpp
@@ -6,16 +6,35 @@
 /*   By: gcesar-n <gcesar-n@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/15 14:47:42 by gcesar-n          #+#    #+#             */
-/*   Updated: 2025/10/20 14:19:12 by gcesar-n         ###   ########.fr       */
+/*   Updated: 2025/10/20 17:10:37 by gcesar-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "utils.hpp"
 
-// bool replace_loop(std::string result, char **argv)
-// {
-	
-// }
+bool replace_loop(std::string content, char **argv)
+{
+	std::ofstream out;
+	int i;
+	int position;
+
+	out.open((std::string(argv[1]) + ".replace").c_str());
+	if (out.fail())
+		return (1);
+	for (i = 0; i < (int)content.size(); i++)
+	{
+		position = content.find(argv[2], i);
+		if (position != -1 && position == i)
+		{
+			out << argv[3];
+			i += std::string(argv[2]).size() - 1;
+		}
+		else
+			out << content[i];
+	}
+	out.close();
+	return (true);
+}
 
 bool check_argc(int argc)
 {
@@ -45,9 +64,7 @@ std::string parse_args(char **argv)
 		in >> std::noskipws >> c;
 
 		if (in.eof())
-		{
 			break;
-		}
 		result += c;
 	}
 	in.close();
--- ex05/Harl.hpp
+++ ex05/Harl.hpp
@@ -1,24 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   utils.hpp                                          :+:      :+:    :+:   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gcesar-n <gcesar-n@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/15 14:48:10 by gcesar-n          #+#    #+#             */
-/*   Updated: 2025/10/20 17:11:03 by gcesar-n         ###   ########.fr       */
+/*   Created: 2025/10/20 17:13:03 by gcesar-n          #+#    #+#             */
+/*   Updated: 2025/10/20 17:13:03 by gcesar-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef UTILS_HPP
-#define UTILS_HPP
-
-#include <iostream>
-#include <fstream>
-#include <cstdlib>
-
-bool check_argc(int argc);
-std::string parse_args(char **argv);
-bool replace_loop(std::string result, char **argv);
-
-#endif
